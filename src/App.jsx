f6d294378042030a16a72260b8eaa9bee54ca1dd
Ariel <arielpenna@gmail.com>
2024-07-31 20:47:24 -0300

    se agrego boton de borrar y se conecto el formulario con la db

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Home from "./Componentes/Home"
-import Alumnos from "./Componentes/InfoAlumnos"
-import Error from './Componentes/Error'
-import { Routes, Route } from "react-router-dom"
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import Home from './Componentes/Home';
+import Alumnos from './Componentes/InfoAlumnos';
+import Error from './Componentes/Error';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [alumnos, setAlumnos] = useState()
-  useEffect(() => {
-    fetch("http://localhost:3000/students").then(res => res.json()).then(data => setAlumnos(data))
-  }, [])
-  console.log(alumnos)
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home data={alumnos} />} />
-        <Route path="/infoalumnos" element={<Alumnos data={alumnos} />} />
-        <Route path="/Error" element={<Error />} />
-      </Routes>
-    </>
-  )
+	const [alumnos, setAlumnos] = useState();
+	const [nuevoAlumno, setNuevoAlumnno] = useState({
+		id: 0,
+		name: '',
+		lastName: '',
+		age: 0,
+		email: '',
+	});
+
+	const handleSubmit = () => {
+		fetch('http://localhost:3000/students', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(nuevoAlumno),
+		});
+	};
+
+	const fetchData = () => {
+		fetch('http://localhost:3000/students')
+			.then((res) => res.json())
+			.then((data) => setAlumnos(data));
+	};
+
+	const helpers = { nuevoAlumno, setNuevoAlumnno, handleSubmit, fetchData };
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Home data={alumnos} helpers={helpers} />} />
+				<Route path='/infoalumnos' element={<Alumnos data={alumnos} />} />
+				<Route path='/Error' element={<Error />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,32 +1,77 @@
-import React from 'react'
+import React from 'react';
 
-function Home({ data }) {
-    return (<>
-        <div>Home</div>
-        <form>
-            <input type='number' id='id' placeholder='ID' />
-            <br />
-            <input id='Nombre' placeholder='Nombre' />
-            <br />
-            <input id='Apellido' placeholder='Apellido' />
-            <br />
-            <input type='number' id='edad' placeholder='Edad' />
-            <br />
-            <input type='mail' id='gmail' placeholder='alumno@gmail.com' />
+function Home({ data, helpers }) {
+	const { nuevoAlumno, setNuevoAlumnno, handleSubmit, fetchData } = helpers;
 
-        </form>
-        <div>
-            <ul>
-                {data?.map((alumno) => {
-                    return <>
-                        <li>{alumno.name}</li>
-                    </>
-                })
-                }
-            </ul>
-        </div>
+	const handleClick = async (alumnoId) => {
+		await fetch(`http://localhost:3000/students/${alumnoId}`, {
+			method: 'DELETE',
+		});
+		fetchData();
+	};
 
-    </>)
+	return (
+		<>
+			<div>Home</div>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={(e) =>
+						setNuevoAlumnno({ ...nuevoAlumno, id: e.target.value })
+					}
+					type='string'
+					id='id'
+					placeholder='ID'
+				/>
+				<br />
+				<input
+					onChange={(e) =>
+						setNuevoAlumnno({ ...nuevoAlumno, name: e.target.value })
+					}
+					id='Nombre'
+					placeholder='Nombre'
+				/>
+				<br />
+				<input
+					onChange={(e) =>
+						setNuevoAlumnno({ ...nuevoAlumno, lastname: e.target.value })
+					}
+					id='Apellido'
+					placeholder='Apellido'
+				/>
+				<br />
+				<input
+					onChange={(e) =>
+						setNuevoAlumnno({ ...nuevoAlumno, age: e.target.value })
+					}
+					type='string'
+					id='edad'
+					placeholder='Edad'
+				/>
+				<br />
+				<input
+					onChange={(e) =>
+						setNuevoAlumnno({ ...nuevoAlumno, email: e.target.value })
+					}
+					type='mail'
+					id='gmail'
+					placeholder='alumno@gmail.com'
+				/>
+				<button type='submit'>Agregar usuario</button>
+			</form>
+			<div>
+				<ul>
+					{data?.map((alumno) => {
+						return (
+							<div style={{ display: 'flex' }}>
+								<li>{alumno.name}</li>
+								<button onClick={() => handleClick(alumno.id)}>Borrar</button>
+							</div>
+						);
+					})}
+				</ul>
+			</div>
+		</>
+	);
 }
 
-export default Home
+export default Home;
